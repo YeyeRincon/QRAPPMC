2f75806f51fdb6ac64f54ba20383ecbf4ca6c9cc
Ing Jeisson Rincon <ing_jeisson_rincon@outlook.com>
2022-08-09 18:28:47 -0500

    inicio

---
+++ app/src/main/java/com/mc/qr/retrofit/DataPrintService.kt
@@ -0,0 +1,20 @@
+package com.mc.qr.retrofit
+
+import com.mc.qr.retrofit.request.RequestLogin
+import com.mc.qr.retrofit.request.RequestPatios
+import com.mc.qr.retrofit.response.ResponseLogin
+import com.mc.qr.retrofit.response.ResponsePatios
+import retrofit2.Call
+import retrofit2.http.*
+
+interface DataPrintService {
+
+    @POST("/api/login")
+    fun login(@Body requestLogin: RequestLogin): Call<ResponseLogin>
+
+    @Headers("Content-Type:application/json; charset=UTF-8")
+    @GET("/api/patios/")
+    fun consupatios(@Body requestPatios: RequestPatios): Call<ResponsePatios>
+
+
+}
